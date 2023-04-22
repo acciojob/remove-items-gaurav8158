52f00ce2b4f9a825737a5481f54f85507c3d5b7a
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-22 23:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById("colorSelect");
+const removeColorBtn = document.getElementById("removeColorBtn");
+
+removeColorBtn.addEventListener("click", function() {
+  const selectedColor = colorSelect.value;
+  const options = colorSelect.options;
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === selectedColor) {
+      colorSelect.removeChild(options[i]);
+      break;
+    }
+  }
+});
